20847f0903490f343d316821397049170c32edcd
ramtan <ramtan.0605@gmail.com>
2026-06-23 13:54:32 +0900

    test

---
+++ resources/views/about.blade.php
@@ -0,0 +1,2 @@
+<h1>hello welcome to laravel</h1>
+
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    $person = [
+        "name" => "Zura",
+        "email" => "zura@gmail.com",
+    ];
+    dd($person);
     return view('welcome');
 });
+
+Route::view('/about',"about");
+
+Route::get('/product/{id?}', function (string $id = null) {
+    return "Work!".$id;
+}) -> whereNumber('id');
